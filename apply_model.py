41b37d57f9b258e6469560c024300b007a634744
Mateo Torres <torresmateo@gmail.com>
2023-10-11 11:30:22 -0400

    added aguments to configure the in/out dirs

--- apply_model.py
+++ apply_model.py
@@ -18,6 +18,7 @@ from src.scoring import bc_scoring, bc_score_names
 from model.config import config_model, config_data
 from model.data_handler import Dataset
 from model.model import Model
+from pathlib import Path
 
 import logging
 
@@ -31,82 +32,99 @@ ch.setLevel(logging.INFO)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-# data parameters
-data_path = "examples/issue_19_04_2023"
 
-# model parameters
-# R3
-# save_path = "model/save/i_v3_0_2021-05-27_14-27"  # 89
-# save_path = "model/save/i_v3_1_2021-05-28_12-40"  # 90
-# R4
-# save_path = "model/save/i_v4_0_2021-09-07_11-20"  # 89
-save_path = "model/save/i_v4_1_2021-09-07_11-21"  # 91
+def run(data_path: str, output_dir: Path) -> None:
+    # data parameters
+    # data_path = "examples/issue_19_04_2023"
+    logger.info(f"output will be saved here: {output_dir=}")
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-# select saved model
-model_filepath = os.path.join(save_path, 'model_ckpt.pt')
-# model_filepath = os.path.join(save_path, 'model.pt')
+    # model parameters
+    # R3
+    # save_path = "model/save/i_v3_0_2021-05-27_14-27"  # 89
+    # save_path = "model/save/i_v3_1_2021-05-28_12-40"  # 90
+    # R4
+    # save_path = "model/save/i_v4_0_2021-09-07_11-20"  # 89
+    save_path = "model/save/i_v4_1_2021-09-07_11-21"  # 91
 
-# add module to path
-if save_path not in sys.path:
-    sys.path.insert(0, save_path)
+    # select saved model
+    model_filepath = os.path.join(save_path, 'model_ckpt.pt')
+    # model_filepath = os.path.join(save_path, 'model.pt')
 
-# load functions
+    # add module to path
+    if save_path not in sys.path:
+        sys.path.insert(0, save_path)
+
+    # define device
+    device = pt.device('cuda' if pt.cuda.is_available() else 'cpu')
+
+    # create model
+    model = Model(config_model)
+
+    # reload model
+    model.load_state_dict(pt.load(model_filepath,
+                                  map_location=device))
+
+    # set model to inference
+    model = model.eval().to(device)
+
+    # find pdb files and ignore already predicted oins
+    pdb_filepaths = glob(os.path.join(data_path, "*.pdb"), recursive=True)
+    pdb_filepaths = [fp for fp in pdb_filepaths if "_i" not in fp]
+
+    # create dataset loader with preprocessing
+    dataset = StructuresDataset(pdb_filepaths, with_preprocessing=True)
+
+    # debug print
+    print(len(dataset))
+
+    # run model on all subunits
+    with pt.no_grad():
+        for subunits, filepath in tqdm(dataset):
+            logger.info(f"{filepath=}")
+            fpath = Path(filepath)
+            # concatenate all chains together
+            structure = concatenate_chains(subunits)
+
+            # encode structure and features
+            X, M = encode_structure(structure)
+            # q = pt.cat(encode_features(structure), dim=1)
+            q = encode_features(structure)[0]
+
+            # extract topology
+            ids_topk, _, _, _, _ = extract_topology(X, 64)
+
+            # pack data and setup sink (IMPORTANT)
+            X, ids_topk, q, M = collate_batch_features([[X, ids_topk, q, M]])
+
+            # run model
+            z = model(X.to(device),
+                      ids_topk.to(device),
+                      q.to(device),
+                      M.float().to(device))
+
+            # for all predictions
+            for i in range(z.shape[1]):
+                # prediction
+                p = pt.sigmoid(z[:, i])
+
+                # encode result
+                structure = encode_bfactor(structure, p.cpu().numpy())
+
+                # save results
+                output_filepath = output_dir / f"{fpath.name}-interface_{i}"
+                logger.info(f"saving prediction into {output_filepath=}")
+                save_pdb(split_by_chain(structure), output_filepath)
 
 
-# define device
-device = pt.device('cuda' if pt.cuda.is_available() else 'cpu')
+if __name__ == "__main__":
+    import argparse
 
-# create model
-model = Model(config_model)
-
-# reload model
-model.load_state_dict(pt.load(model_filepath, map_location=pt.device("cpu")))
-
-# set model to inference
-model = model.eval().to(device)
-
-# find pdb files and ignore already predicted oins
-pdb_filepaths = glob(os.path.join(data_path, "*.pdb"), recursive=True)
-pdb_filepaths = [fp for fp in pdb_filepaths if "_i" not in fp]
-
-# create dataset loader with preprocessing
-dataset = StructuresDataset(pdb_filepaths, with_preprocessing=True)
-
-# debug print
-print(len(dataset))
-
-# run model on all subunits
-with pt.no_grad():
-    for subunits, filepath in tqdm(dataset):
-        logger.info(f"{subunits=} {filepath=}")
-        # concatenate all chains together
-        structure = concatenate_chains(subunits)
-
-        # encode structure and features
-        X, M = encode_structure(structure)
-        # q = pt.cat(encode_features(structure), dim=1)
-        q = encode_features(structure)[0]
-
-        # extract topology
-        ids_topk, _, _, _, _ = extract_topology(X, 64)
-
-        # pack data and setup sink (IMPORTANT)
-        X, ids_topk, q, M = collate_batch_features([[X, ids_topk, q, M]])
-
-        # run model
-        z = model(X.to(device),
-                  ids_topk.to(device),
-                  q.to(device),
-                  M.float().to(device))
-
-        # for all predictions
-        for i in range(z.shape[1]):
-            # prediction
-            p = pt.sigmoid(z[:, i])
-
-            # encode result
-            structure = encode_bfactor(structure, p.cpu().numpy())
-
-            # save results
-            output_filepath = filepath[:-4]+'_i{}.pdb'.format(i)
-            save_pdb(split_by_chain(structure), output_filepath)
+    parser = argparse.ArgumentParser(
+        description="Reads Yulab's PDBs and prepares the files for BIPSPI+")
+    parser.add_argument("-i", "--input-dir", required=True,
+                        help="A directory containing PDF files")
+    parser.add_argument("-o", "--output-dir", required=True,
+                        help="A path to write the renamed PDB files")
+    args = parser.parse_args()
+    run(args.input_dir, args.output_dir)
